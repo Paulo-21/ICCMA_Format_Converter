21457a691b6f0956d8d6dd5b135de847445e5ae9
Paulo-21 <paul.cibier@gmail.com>
2024-01-11 01:06:26 +0100

    Write once for better performance

--- src/converter.rs
+++ src/converter.rs
@@ -4,11 +4,12 @@ use std::io::Write;
 
 pub fn convert_APX_to_ICCMA2023(mut filepath : String) {
     let content  = fs::read_to_string(filepath.clone()).unwrap();
-    
+     println!("Converting \"{}\" to \"{}.af\" ...", filepath, filepath);
     filepath.push_str(".af");
     let mut f = File::create(filepath).unwrap();
     let mut nb_arg = 0;
     let mut begin = true;
+    let mut dest = String::with_capacity(content.len());
     for line in content.lines() {
         if line.starts_with("arg") {
             nb_arg+=1;
@@ -18,23 +19,31 @@ pub fn convert_APX_to_ICCMA2023(mut filepath : String) {
                 begin = false;
                 let mut buf = String::from("p af ");
                 buf.push_str(nb_arg.to_string().as_str());
-                f.write(buf.as_bytes());
-                f.write(&[b'\n']);
+                dest.push_str(&buf);
+                /*let _ = f.write(buf.as_bytes());
+                let _ = f.write(&[b'\n']);*/
+                dest.push('\n');
             }
-            let mut buff = line.strip_prefix("att(a").unwrap();
+            let buff = line.strip_prefix("att(a").unwrap();
             let buff2 = buff.strip_suffix(").").unwrap();
             let buff2 = buff2.replace(",a", " ");
-            f.write(buff2.as_bytes()).unwrap();
-            f.write(&[b'\n']);
+            dest.push_str(&buff2);
+            dest.push('\n');
+            //f.write(buff2.as_bytes()).unwrap();
+            //let _ = f.write(&[b'\n']);
         }
         else if line.starts_with('#') {
-            f.write(line.as_bytes()).unwrap();
+            //f.write(line.as_bytes()).unwrap();
+            dest.push_str(&line);
         }
     }
+    f.write_all(&dest.as_bytes()).unwrap();
 }
 pub fn convert_ICCMA2023_to_APX(mut filepath : String) {
     let content  = fs::read_to_string(filepath.clone()).expect("BIZARRE");
+    println!("Converting \"{}\" to \"{}.apx\" ...", filepath, filepath);
     filepath.push_str(".apx");
+    let mut dest = String::with_capacity(content.len()+(content.len()/2));
     let mut f = File::create(filepath).unwrap();
     let mut iter = content.lines();
     let nb_arg : i32 = iter.next().unwrap().split_ascii_whitespace().nth(2).unwrap().parse().unwrap();
@@ -43,19 +52,28 @@ pub fn convert_ICCMA2023_to_APX(mut filepath : String) {
         let mut buff = String::from("arg(a");
         buff.push_str(i.to_string().as_str());
         buff.push_str(").");
-        f.write(buff.as_bytes()).unwrap();
-        let _ = f.write(&[b'\n']);
+        dest.push_str(&buff);
+        dest.push('\n');
+        //f.write(buff.as_bytes()).unwrap();
+        //let _ = f.write(&[b'\n']);
     }
     for line in iter {
         let split : Vec<&str> = line.split_ascii_whitespace().collect();
+        if split.is_empty() {
+            continue;
+        }
         let n1 = split[0];
         let n2 = split[1];
-        let mut buff = String::from("att(a");
+        let mut buff = String::with_capacity(20);
+        buff.push_str("att(a");
         buff.push_str(n1.to_string().as_str());
         buff.push_str(",a");
         buff.push_str(n2.to_string().as_str());
         buff.push_str(").");
-        f.write(buff.as_bytes()).unwrap();
-        let _ = f.write(&[b'\n']);
+        dest.push_str(&buff);
+        dest.push('\n');
+        //f.write(buff.as_bytes()).unwrap();
+        //let _ = f.write(&[b'\n']);
     }
+    f.write_all(&dest.as_bytes()).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,35 @@
-use std::env;
+use std::{env, process::exit};
 mod converter;
 use converter::*;
 
 fn print_help() {
     println!("Made by Paul Cibier");
+    println!();
     println!("Usage : ./command mode filePath");
+    println!();
     println!("Mode 1 : APX -> ICCMA 2023");
     println!("Mode 2 : ICCMA 2023 -> APX");
 }
 fn main() {
     println!("ICCMA Format Converter");
-    let mut arg = env::args().collect::<Vec<String>>();
+    let arg = env::args().collect::<Vec<String>>();
     if arg.len() <= 2 {
         print_help();
     }
-    println!("{:?}", arg);
-    let mode = arg[1].parse().unwrap();
-    let filepath = arg[2].clone();
-    match mode {
-        1 => {
-            convert_APX_to_ICCMA2023(filepath);
-        },
-        2 => {
-            convert_ICCMA2023_to_APX(filepath);
-        },
-        _=> {
-            println!("This mode is not suported ");
-            print_help();
+    else {
+        let mode = arg[1].parse().unwrap();
+        let filepath = arg[2].clone();
+        match mode {
+            1 => {
+                convert_APX_to_ICCMA2023(filepath);
+            },
+            2 => {
+                convert_ICCMA2023_to_APX(filepath);
+            },
+            _=> {
+                println!("This mode is not suported ");
+                print_help();
+            }
         }
     }
 
